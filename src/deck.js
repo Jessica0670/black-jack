6ae486d892755ce7a83dfa5940fe15a6c0c21e80
Jessica Thompson <jthompson0670@gmail.com>
2018-09-21 10:12:21 -0700

    shuffle added

--- src/deck.js
+++ src/deck.js
@@ -22,10 +22,54 @@ class Deck extends Component {
     this.setState({deck: deck})
   }
 
+  shuffle() {
+    const deck = this.state.deck;
+    let m = deck.length, i;
+
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+
+      [deck[m], deck[i]] = [deck[i], deck[m]];
+    }
+  }
+
+  // deal() {
+  //   let card1 = this.deck.pop()
+  //   let card2 = this.deck.pop()
+  //   this.hand.push(card1, card2);
+  //   console.log(this.deck, 'remaining cards', this.deck.length)
+  //   return this.hand;
+  // }
+
+  // hit() {
+  //   let card = this.deck.pop()
+  //   this.hand.push(card)
+  //   return this.hand
+  // }
+
+
+  // reset(){
+  //   this.deck = [];
+
+  //   const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+  //   const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+  //   for (let suit in suits) {
+  //     for (let value in values) {
+  //       this.deck.push(values[value] + ' of ' + suits[suit]);
+  //     }
+  //   }
+  // }
+
   render() {
     return (
       <div className="Deck">
-        {this.state.deck}
+      <button>Deal</button>
+      <button>Hit</button>
+      <button>Stay</button>
+
+      <button onClick={()=>this.shuffle()}>Shuffle</button>
+      <button>Reset</button>
       </div>
     );
   }
